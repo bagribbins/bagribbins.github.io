6cf146ecf1b80d7b2a3ff6359b189b2694ee1c38
bagribbins <b.a.gribbins@gmail.com>
2020-12-07 19:09:54 -0500

    Adjusted javascript for iPhone functionality.  Modified some experience.html

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,8 +1,10 @@
+$('#expand_data').css('cursor', 'pointer');
 $('#expand_data').click(function () {
     $('.collapse').removeClass('show');
     $('.collapse').toggleClass('show');
 })
 
+$('#collapse_data').css('cursor', 'pointer');
 $('#collapse_data').click(function () {
     $('.show').removeClass('show');
 })
